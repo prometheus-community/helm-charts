1b7041c1f025e729fc9657db3fb6026c85d695b9
Stanislav Khalash <stanislav.khalash@sap.com>
2022-02-10 17:27:15 +0100

    Fix condition
    Signed-off-by: Stanislav Khalash <stanislav.khalash@sap.com>

--- charts/kube-prometheus-stack/hack/sync_prometheus_rules.py
+++ charts/kube-prometheus-stack/hack/sync_prometheus_rules.py
@@ -75,7 +75,7 @@ condition_map = {
     'k8s.rules': ' .Values.defaultRules.rules.k8s',
     'kube-apiserver-availability.rules': ' .Values.kubeApiServer.enabled .Values.defaultRules.rules.kubeApiserverAvailability',
     'kube-apiserver-burnrate.rules': ' .Values.kubeApiServer.enabled .Values.defaultRules.rules.kubeApiserverBurnrate',
-    'kube-apiserver-histogram.rules': ' .Values.kubeApiServer.enabled .Values.defaultRules.rules.kubeApiserverSlos',
+    'kube-apiserver-histogram.rules': ' .Values.kubeApiServer.enabled .Values.defaultRules.rules.kubeApiserverHistogram',
     'kube-apiserver-slos': ' .Values.kubeApiServer.enabled .Values.defaultRules.rules.kubeApiserverSlos',
     'kube-prometheus-general.rules': ' .Values.defaultRules.rules.kubePrometheusGeneral',
     'kube-prometheus-node-recording.rules': ' .Values.defaultRules.rules.kubePrometheusNodeRecording',
