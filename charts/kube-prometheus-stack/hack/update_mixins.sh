c067c6b586e413b08fe7d23342d3b34bbdab7afc
Jan-Otto Kröpke <github@jkroepke.de>
2024-02-26 19:23:15 +0100

    update_mixins.sh aktualisieren
    Co-authored-by: Gabriel Martinez <19713226+GMartinez-Sisti@users.noreply.github.com>
    Signed-off-by: Jan-Otto Kröpke <github@jkroepke.de>

--- charts/kube-prometheus-stack/hack/update_mixins.sh
+++ charts/kube-prometheus-stack/hack/update_mixins.sh
@@ -29,5 +29,5 @@ source "${SCRIPT_DIR}/tmp/venv/bin/activate"
 
 pip3 install -r "${SCRIPT_DIR}/requirements.txt"
 
-./sync_grafana_dashboards.py
-./sync_prometheus_rules.py
+"${SCRIPT_DIR}/sync_grafana_dashboards.py"
+"${SCRIPT_DIR}/sync_prometheus_rules.py"
