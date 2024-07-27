c609e6204639d1c1aef140d4c07acc1064f66469
Jan-Otto Kröpke <mail@jkroepke.de>
2024-07-27 08:56:03 +0200

    [kube-prometheus-stack] Fix mixins generation and fix update on mac
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- charts/kube-prometheus-stack/hack/update_mixins.sh
+++ charts/kube-prometheus-stack/hack/update_mixins.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+sedi () {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "$@"
+  else
+    sed -i "$@"
+  fi
+}
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 trap 'rm -rf "${SCRIPT_DIR}/tmp"' EXIT
@@ -17,8 +25,8 @@ for REPO_PATH in "${SCRIPT_DIR}/tmp/"*; do
   SHA=$(git -C "$REPO_PATH" log -1 --pretty=format:"%H")
   REPO_NAME=$(basename "$REPO_PATH")
   echo "Updating $REPO_NAME to $SHA"
-  sed -i -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_grafana_dashboards.py"
-  sed -i -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_prometheus_rules.py"
+  sedi -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_grafana_dashboards.py"
+  sedi -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_prometheus_rules.py"
 done
 
 export PIP_DISABLE_PIP_VERSION_CHECK=1
