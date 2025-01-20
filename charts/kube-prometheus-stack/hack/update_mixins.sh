8c77fe59e4462a0e0d45fc9d2f80fa4677de2c78
Jan-Otto Kröpke <joe@cloudeteer.de>
2025-01-21 00:00:42 +0100

    fix lint
    Signed-off-by: Jan-Otto Kröpke <joe@cloudeteer.de>

--- charts/kube-prometheus-stack/hack/update_mixins.sh
+++ charts/kube-prometheus-stack/hack/update_mixins.sh
@@ -19,11 +19,11 @@ git clone --depth 1 --quiet https://github.com/kubernetes-monitoring/kubernetes-
 git clone --depth 1 --quiet https://github.com/etcd-io/etcd.git "${SCRIPT_DIR}/tmp/etcd"
 
 for REPO_PATH in "${SCRIPT_DIR}/tmp/"*; do
-	SHA=$(git -C "$REPO_PATH" log -1 --pretty=format:"%H")
-	REPO_NAME=$(basename "$REPO_PATH")
-	echo "Updating $REPO_NAME to $SHA"
-	_sed_i -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_grafana_dashboards.py"
-	_sed_i -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_prometheus_rules.py"
+  SHA=$(git -C "$REPO_PATH" log -1 --pretty=format:"%H")
+  REPO_NAME=$(basename "$REPO_PATH")
+  echo "Updating $REPO_NAME to $SHA"
+  _sed_i -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_grafana_dashboards.py"
+  _sed_i -e "s/'ref.$REPO_NAME'.*:.*'.*'/'ref.$REPO_NAME': '$SHA'/" "${SCRIPT_DIR}/sync_prometheus_rules.py"
 done
 
 export PIP_DISABLE_PIP_VERSION_CHECK=1
