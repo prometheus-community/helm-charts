f389e905bb92ec985df07b8abcb1679420394464
Ethan Dean <ecdean88@gmail.com>
2025-03-05 18:02:26 -0500

    [kube-prometheus-stack] Add interval field for default grafana dashboards
    Signed-off-by: Ethan Dean <ecdean88@gmail.com>

--- charts/kube-prometheus-stack/hack/sync_grafana_dashboards.py
+++ charts/kube-prometheus-stack/hack/sync_grafana_dashboards.py
@@ -29,11 +29,11 @@ def change_style(style, representer):
 
 refs = {
     # https://github.com/prometheus-operator/kube-prometheus
-    'ref.kube-prometheus': '997df34c70eb3eebc12b9839c972b01a892f9d73',
+    'ref.kube-prometheus': 'baf3c7a71ec9f889644231f677f8708791d38293',
     # https://github.com/kubernetes-monitoring/kubernetes-mixin
-    'ref.kubernetes-mixin': '4ff562d5e8145940cf355f62cf2308895c4dca81',
+    'ref.kubernetes-mixin': '1fa3b6731c93eac6d5b8c3c3b087afab2baabb42',
     # https://github.com/etcd-io/etcd
-    'ref.etcd': '498bbc598224c46d27a55a483c0c428a8deab4ca',
+    'ref.etcd': 'f0a1ec501b03da6090bfbf3a1cbe078f952c46f9',
 }
 
 # Source files list
@@ -206,6 +206,11 @@ def patch_json_set_editable_as_variable(content):
     return re.sub(r'"editable"\s*:\s*(?:true|false)', '"editable":`}}{{ .Values.grafana.defaultDashboardsEditable }}{{`', content, flags=re.IGNORECASE)
 
 
+def patch_json_set_interval_as_variable(content):
+    # content is no more in json format, so we have to replace using regex
+    return re.sub(r'"interval"\s*:\s*"(?:\\.|[^\"])*"', '"interval":"`}}{{ .Values.grafana.defaultDashboardsInterval }}{{`"', content, flags=re.IGNORECASE)
+
+
 def jsonnet_import_callback(base, rel):
     # rel_base is the path relative to the current cwd.
     # see https://github.com/prometheus-community/helm-charts/issues/5283
@@ -238,6 +243,7 @@ def write_group_to_file(resource_name, content, url, destination, min_kubernetes
     content = patch_dashboards_json(content, multicluster_key)
     content = patch_json_set_timezone_as_variable(content)
     content = patch_json_set_editable_as_variable(content)
+    content = patch_json_set_interval_as_variable(content)
 
     filename_struct = {resource_name + '.json': (LiteralStr(content))}
     # rules themselves
