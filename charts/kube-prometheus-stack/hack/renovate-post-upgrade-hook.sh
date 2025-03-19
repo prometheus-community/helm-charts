0b60c665c89caaa9bb1d02520c8ce2c95e84daed
Jan-Otto Kröpke <mail@jkroepke.de>
2025-03-19 09:20:26 +0100

    [kube-prometheus-stack] install jb command inside renovate-post-upgrade-hook.sh (#5434)
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>
    Signed-off-by: Louis <louis.paret@mindee.co>

--- charts/kube-prometheus-stack/hack/renovate-post-upgrade-hook.sh
+++ charts/kube-prometheus-stack/hack/renovate-post-upgrade-hook.sh
@@ -5,9 +5,23 @@ set -euo pipefail
 SCRIPT_DIR=$(cd -- "$(dirname -- "${0}")" &>/dev/null && pwd)
 
 if ! which jb &>/dev/null; then
-  echo "'jb' command not found
-Install jsonnet-bundler from https://github.com/jsonnet-bundler/jsonnet-bundler"
-  exit 1
+  if ! which go &>/dev/null; then
+    echo "'jb' command not found"
+    echo "Install jsonnet-bundler from https://github.com/jsonnet-bundler/jsonnet-bundler"
+    exit 1
+  fi
+
+  echo "'jb' command not found. Try to install it from github.com/jsonnet-bundler/jsonnet-bundler"
+
+  go install -a github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb@latest
+  PATH="$(go env GOPATH)/bin:$PATH"
+  export PATH
+
+  if ! which jb &>/dev/null; then
+    echo "'jb' command not found"
+    echo "Install jsonnet-bundler from https://github.com/jsonnet-bundler/jsonnet-bundler"
+    exit 1
+  fi
 fi
 
 rm -rf "${SCRIPT_DIR}/tmp"
