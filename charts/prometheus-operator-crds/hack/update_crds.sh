6a5a72949b8297f4a33f0a22e70941806a14f702
Simon Berz <simon@berz.me>
2025-04-11 12:00:35 +0200

    fixup! [prometheus-operator-crds] add opt-out for CRDs
    Signed-off-by: Simon Berz <simon@berz.me>

--- charts/prometheus-operator-crds/hack/update_crds.sh
+++ charts/prometheus-operator-crds/hack/update_crds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 if [[ $(uname -s) = "Darwin" ]]; then
-    VERSION="$(grep ^appVersion "${SCRIPT_DIR}/../Chart.yaml" | sed 's/appVersion: //g')"
+  VERSION="$(grep ^appVersion "${SCRIPT_DIR}/../Chart.yaml" | sed 's/appVersion: //g')"
 else
-    VERSION="$(grep ^appVersion "${SCRIPT_DIR}/../Chart.yaml" | sed 's/appVersion:\s//g')"
+  VERSION="$(grep ^appVersion "${SCRIPT_DIR}/../Chart.yaml" | sed 's/appVersion:\s//g')"
 fi
 
 CRDS=(
@@ -23,29 +23,29 @@ CRDS=(
 )
 
 for line in "${CRDS[@]}"; do
-    CRD=$(echo "${line%%:*}" | xargs)
-    SOURCE=$(echo "${line##*:}" | xargs)
-    DESTINATION="crd-${CRD}".yaml
+  CRD=$(echo "${line%%:*}" | xargs)
+  SOURCE=$(echo "${line##*:}" | xargs)
+  DESTINATION="crd-${CRD}".yaml
 
-    URL="https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/$VERSION/example/prometheus-operator-crd/$SOURCE"
+  URL="https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/$VERSION/example/prometheus-operator-crd/$SOURCE"
 
-    echo -e "Downloading Prometheus Operator CRD with Version ${VERSION}:\n${URL}\n"
+  echo -e "Downloading Prometheus Operator CRD with Version ${VERSION}:\n${URL}\n"
 
-    echo "# ${URL}" > "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
+  echo "# ${URL}" >"${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
 
-    if ! curl --silent --retry-all-errors --fail --location "${URL}" >> "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"; then
-      echo -e "Failed to download ${URL}!"
-      exit 1
-    fi
+  if ! curl --silent --retry-all-errors --fail --location "${URL}" >>"${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"; then
+    echo -e "Failed to download ${URL}!"
+    exit 1
+  fi
 
-    # Update or insert annotations block
-    if yq -e '.metadata.annotations' "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}" >/dev/null; then
-      sed -i '/^  annotations:$/a {{- with .Values.annotations }}\n{{- toYaml . | nindent 4 }}\n{{- end }}' "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
-    else
-      sed -i '/^metadata:$/a {{- with .Values.annotations }}\n  annotations:\n{{- toYaml . | nindent 4 }}\n{{- end }}' "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
-    fi
+  # Update or insert annotations block
+  if yq -e '.metadata.annotations' "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}" >/dev/null; then
+    sed -i '/^  annotations:$/a {{- with .Values.annotations }}\n{{- toYaml . | nindent 4 }}\n{{- end }}' "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
+  else
+    sed -i '/^metadata:$/a {{- with .Values.annotations }}\n  annotations:\n{{- toYaml . | nindent 4 }}\n{{- end }}' "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
+  fi
 
-    # Insert enable option
-    sed -i "1i\{{- if .Values.${CRD}.enabled -}}" "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
-    echo "{{- end -}}" >> "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
+  # Insert enable option
+  sed -i "1i\{{- if .Values.${CRD}.enabled -}}" "${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
+  echo "{{- end -}}" >>"${SCRIPT_DIR}/../charts/crds/templates/${DESTINATION}"
 done
